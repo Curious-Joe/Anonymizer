8282a34d32093a5fce1b4607ce6ffb0be4cda5d4
Hossain <ahossa1@ilstu.edu>
2022-04-05 15:43:29 -0500

    added setup up files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'Anonymizer',
+    packages = ['Anonymizer'],
+    version = 'v1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'Arafath Hossain',
+    author_email = '',
+    url = 'https://github.com/Curious-Joe/Anonymizer.git',
+    download_url = 'https://github.com/Curious-Joe/Anonymizer/archive/refs/tags/v1.0.0.tar.gz',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
